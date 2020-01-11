3bbd655eda533bcbed0c343d69c04da356fb4c1b
umartunes <s.umarahmad@gmail.com>
2020-01-11 19:09:51 +0500

    Lecture code for session: 11-Jan-2020

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,38 +1,73 @@
 import React, { Component } from 'react';
 
+
+class DisplayTodos extends Component {
+    render() {
+        return <div className="DisplayTodos">
+            <ul>
+                {this.props.abc.map((todo, index) => {
+                    return <li key={index}>
+                        {todo === "Breakfast" ? `I don't like eggs` : todo}
+                    </li>
+                })}
+            </ul>
+        </div>
+    }
+}
+
+class AddTodo extends Component {
+
+    state = {
+        todoField: ""
+    }
+
+    handleChange = (event) => {
+
+        this.setState({ todoField: event.target.value })
+    }
+
+    handleClick = () => {
+
+        let todo = this.state.todoField
+
+        if (todo !== null) {
+
+            this.props.addToList(todo)
+            this.setState({ todoField: "" })
+
+        }
+
+    }
+
+    render() {
+        return <div className="AddTodo">
+            <input type="text" value={this.state.todoField} placeholder="Enter Todo" onChange={this.handleChange} />
+            <button onClick={this.handleClick}>Add Todo</button>
+        </div>
+    }
+
+}
+
 class Content extends Component {
 
     state = {
         todos: ["Offering Prayer", "Morning Walk", "Breakfast"]
     }
 
-    handleClick = () => {
-
-        let todo = prompt("Enter Todo")
-
-        if (todo !== null) {
-
-            let oldTodos = this.state.todos.slice()
-
-            oldTodos.push(todo)
-
-            this.setState({ todos: oldTodos })
-        }
-
+    addToList = (todo) => {
+        this.setState({ todos: [...this.state.todos, todo] })
     }
 
     render() {
 
         return <div className="Content">
+
             <h2>Todos</h2>
-            <ul>
-                {this.state.todos.map((todo, index) => {
-                    return <li key={index}>
-                        {todo === "Breakfast" ? `I don't like eggs` : todo}
-                    </li>
-                })}
-            </ul>
-            <button onClick={this.handleClick}>Add Todo</button>
+
+            <DisplayTodos abc={this.state.todos} />
+
+            <AddTodo addToList={this.addToList} />
+            <div style={{ clear: 'both' }}></div>
         </div>
 
     }
